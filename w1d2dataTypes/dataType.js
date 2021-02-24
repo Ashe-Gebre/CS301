1b5eccffd68c3d9a1e211b257085264a043bf86e
Ashenafi Gebretekle <ashenafiaynalemg@gmail.com>
2021-02-23 21:21:58 -0600

    updated commit

--- w1d2dataTypes/dataType.js
+++ w1d2dataTypes/dataType.js
@@ -30,12 +30,13 @@
             const mileageInMilesPerGallon = ((endingOdometerReadings - beginningOdometerReadings)/NumberGallonGasUsed);
             console.log(mileageInMilesPerGallon + " miles per gallon.");
 //QUESTION FIVE
-            const endingOdometerReadings = prompt("Enter ending odometer readings: ");
-            const beginningOdometerReadings = prompt("Enter beginning odometer readings: ");
-            const NumberGallonGasUsed = prompt("Enter the number of gallon gas used: ");
+            const ageOfUser = prompt("Enter your age: ");
+            const maxHeartRate = 220;
+            const yourMaxHeartRate = maxHeartRate - ageOfUser;
+            const slowestRate = 0.65 * yourMaxHeartRate;
+            const fastestRate = 0.85 * yourMaxHeartRate;
+            console.log (slowestRate + " & " + fastestRate + " are your lowest & fastest rates during exercise to strengthen your heart respectively");
 
-            const mileageInMilesPerGallon = ((endingOdometerReadings - beginningOdometerReadings)/NumberGallonGasUsed);
-            console.log(mileageInMilesPerGallon + " miles per gallon.");
 
 
 
