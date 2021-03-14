e8fd76c3061827e5302e46e19302b3000e034b84
Ashenafi Gebretekle <ashenafiaynalemg@gmail.com>
2021-03-14 13:19:30 -0500

    Update finalReview.js

--- w3d6FinalReview/finalReview.js
+++ w3d6FinalReview/finalReview.js
@@ -1,24 +1,86 @@
-let a = 5;
-let b = 7;
-console.log(a,b);
-swap1(a,b);
-console.log(a,b)
+"use strict";
+/* 1.	Write a JavaScript function isSumOfPrimesEven that takes
+an array of positive integers as an argument and returns true if the 
+sum of all the prime elements in the given array is even otherwise 
+returns false. If the array is empty or if the array does not contain 
+any prime element it should return false. */
 
-let arr1 = [1,2,3];
-console.log(arr1);
-swap2(arr1, 0, 2);
-console.log(arr1);
-
-function swap1(a,b){  //swapping primitives (which are values)
-    console.log(a, b);
-    let temp = a;
-    a=b;
-    b=temp;
-    console.log(a, b);
+/**
+ * 
+ * @param {Array} arr of pos integers
+ * @returns {boolean} true if sum of all the prime elements in the given array is even
+ */
+function isSumOfPrimesEven(arr) {
+    let sum = 0;
+    for (const element of arr) {
+        if (isPrime(element)) {
+            sum += element;
+        }
+    }
+    if (sum % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function swap2(arr2, i, j){//swapping object properties
-    let temp = arr2[i];
-    arr2[i]= arr2[j];
-    arr2[j]= temp;
+/**
+ * 
+ * @param {number} number is a pos integer
+ * @returns {boolean} true if prime
+ * this is a "stub" -- it sort of works for a quick test
+ */
+function isPrime(num) {
+
+    if (num === 2) {
+        return true;
+    }
+    if (num < 2) {
+        return false;
+    }
+    for (let i = 3; i < num; i++) {
+
+        if (num % i === 0) {
+            return false;
+
+        }
+
+    }
+
+    return true;
 }
+console.log("expect false: ", isSumOfPrimesEven([1, 2, 2, 3, 9]));
+console.log("expect true: ", isSumOfPrimesEven([1, 2, 3, 4, 5, 6]));
+
+
+
+/* 4.	Write a function that takes a string parameter 
+and returns the middle character of the string. If string is of 
+even length then function should return the middle two characters.
+*/
+
+/**
+ * 
+ * @param {string} str any string
+ * @returns {string} the middle character
+ * process - get length and divide by 2
+ *  - if divisible by 2 then return middle 2 , else just the middle one
+ */
+function middle(str) {
+    const length = str.length;
+    const midpoint = Math.floor(length / 2);
+
+    if (length % 2 === 0) {
+        return str[midpoint - 1] + str[midpoint];
+    } else {
+        return str[midpoint];
+    }
+}
+
+console.log("expect b :  ", middle("abc"));
+console.log("expect bc :  ", middle("abcd"));
+
+
+// 6- output------------10-------
+
+// 7-  output------------5-------
