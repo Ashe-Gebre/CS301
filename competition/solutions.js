e30042c35c44cc341a23c67442927a36ffc42be4
Ashenafi Gebretekle <ashenafiaynalemg@gmail.com>
2021-03-18 11:50:17 -0500

    commit

--- competition/solutions.js
+++ competition/solutions.js
@@ -1,46 +1,186 @@
-"use strict"; 
-const assert = require("assert");
-const solutions = require("./com.js");
+"use strict";
+// eslint-disable-next-line require-jsdoc
 
-describe("divisibleSumPairs", function () {
-    it("for arr= [1,2,3,4,5,6] and k=5 result should be 3", function () {
-        const arr = [1, 2, 3, 4, 5, 6];
-        assert.strictEqual(solutions.divisibleSumPairs(arr, 5), 3);
-    });
-});
 
-describe("mostFrequentSight", function () {
-    it("for arr=[1,1,2,2,3] result should be 1", function () {
-        const arr = [1, 1, 2, 2, 3];
-        assert.strictEqual(solutions.mostFrequentSight(arr), 1);
-    });
-});
+//ASHE GROUP---smart group lol
+//Ashenafi
+//Connor
+//Ghana
+//Henok
 
-describe("countSocksPairs", function () {
-    it("for arr=[1,2,1,2,1,3,2] result should be 2", function () {
-        const arr = [1, 2, 1, 2, 1, 3, 2];
-        assert.strictEqual(solutions.countSocksPairs(arr), 2);
-    });
-});
 
-describe("computeHeight", function () {
-    it("for initial height of 1 and 4 cycles, height should be 7", function () {
-        assert.strictEqual(solutions.computeHeight(1, 4), 7);
-    });
-});
+exports.divisibleSumPairs = divisibleSumPairs;
+exports.mostFrequentSight = mostFrequentSight;
+exports.countSocksPairs = countSocksPairs;
+exports.computeHeight = computeHeight;
+exports.computeCharges = computeCharges;
+exports.findMinDistance = findMinDistance;
 
-describe("computeCharges", function () {
-    it("first report for 2 customers", function () {
-        const charges = [{ custId: 1, charges: [5, 7, 3] }, { custId: 2, charges: [20, 60, 50, 30] }];
-        const report = [{ custId: 1, average: 5, maximum: 7 }, { custId: 2, average: 40, maximum: 60 }];
+//Q1
+/**
+* 
+* @param {Number} count is number
+* @returns {Number} counted number output
+*/
+function divisibleSumPairs(arr, k) {
+    let count = 0;
 
-        assert.deepStrictEqual(solutions.computeCharges(charges), report);
-    });
-});
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arr.length; j++) {
 
-describe("findMinDistance", function () {
-    it("min distance for arr = [3,2,1,2,3] should be 2", function () {
-        const arr = [3, 2, 1, 2, 3];
-        assert.strictEqual(solutions.findMinDistance(arr), 2);
-    });
-});
+
+            if ((arr[i] + arr[j]) % k == 0 && i < j)
+                count++;
+
+        }
+
+    }
+    return count;
+}
+
+
+//Q2
+/**
+* 
+* @param {Number} count is number
+* @returns {Array} counted number output
+*/
+function mostFrequentSight(array) {
+    for (const value of array) {
+
+        const absValue = Math.abs(value);
+
+        if (array[absValue - 1] < 0) {
+            return absValue;
+        }
+
+        array[absValue - 1] *= -1;
+    }
+    return -1;
+}
+
+
+//Q3---
+/**
+* 
+* @param {Array} arr is array
+* @returns {Number} counted number output
+*/
+function countSocksPairs(arr) {
+    let n = 10;
+
+    let count = 0, number = 0;
+
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if (count % 2 == 0)
+            number++;
+
+    }
+    return number;
+}
+
+
+
+//4 
+// eslint-disable-next-line require-jsd
+function computeHeight(height, cycle)
+{
+     let no;
+         cycle = 1;
+         height = 1;
+         
+        
+        while (cycle <= no){
+          if(cycle % 2 !== 0 ){
+            height *= 2*no;
+            height--
+          }else{
+            height++;
+          }
+          cycle++
+    
+        
+        return height;
+    }      
+}
+
+
+//5-
+// eslint-disable-next-line require-jsd
+function computeCharges(array) {
+    let max1 = 0;
+    let max2 = 0;
+    let average1 = 0;
+    let average2 = 0;
+    let custId1;
+    let custId2;
+    let custId;
+    let charges;
+    for ({ custId, charges } of array) {
+
+        if (custId === 1) {
+            max1 = getMax(charges);
+            average1 = getAverage(charges);
+            custId1 = custId;
+        }
+
+        else if (custId === 2) {
+            max2 = getMax(charges);
+            average2 = getAverage(charges);
+            custId2 = custId;
+        }
+
+    }
+    return [{ average: average1, custId: custId1, maximum: max1 }, { average: average2, custId: custId2, maximum: max2 }];
+};
+
+function getAverage(arr) {
+    let total = 0;
+    let average = 0;
+    for (const el of arr) {
+        total = total + el;
+    }
+    average = total / arr.length;
+    return average;
+}
+
+function getMax(arr) {
+    const max = Math.max(...arr);
+    return max;
+    }
+
+
+
+//Q 6
+// eslint-disable-next-line require-jsd
+function findMinDistance(arr) {
+    const dups = findDuplicates(arr);
+    return dups;
+}
+
+function findDuplicates(array) {
+    let minSecondIndex = array.length;
+
+    for (let i = 0; i < array.length; i++) {
+        const value = array[i];
+        for (let j = i + 1; j < array.length; j++) {
+            const valueToCompare = array[j];
+            if (valueToCompare === value) {
+                minSecondIndex = Math.min(minSecondIndex, j);
+            }
+        }
+    }
+    if (minSecondIndex === array.length) return -1;
+
+    return array[minSecondIndex];
+}
+
+
+
+
